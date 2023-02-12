07091073777fa38c0cd6e7fd8c9499f1bd83c6ab
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-02-12 22:38:47 +0100

    regression

--- regression.py
+++ regression.py
@@ -1,14 +1,17 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-x_m = np.array([170, 175, 180, 185, 190]).reshape(-1, 1)
-y_m = np.array([60, 70, 78, 87, 96]).reshape(-1, 1)
+data = pd.read_csv("data_set.csv")
+
+x_m = np.array([150, 155, 160, 165, 170, 175, 180, 185, 190]).reshape(-1, 1)
+y_m = np.array([60, 65, 70, 75, 80, 85, 90, 95, 100]).reshape(-1, 1)
 
 model = LinearRegression()
 model.fit(x_m, y_m)
 
-x = np.linspace(170, 190, 50).reshape(-1, 1)
+x = np.linspace(150, 190, 50).reshape(-1, 1)
 y = model.predict(x)
 
 print(f"M: {model.coef_}\tQ: {y[0][0] - model.coef_ * x[0][0]}")
@@ -21,12 +24,10 @@ plt.grid(True)
 plt.scatter(x_m, y_m, c="blue")
 plt.plot(x, y, "b--")
 
-x_f = np.array([150, 155, 160, 170, 180]).reshape(-1, 1)
-y_f = np.array([40, 47, 50, 55, 65]).reshape(-1, 1)
+x_f = np.array([150, 155, 160, 165, 170, 175, 180, 185, 190]).reshape(-1, 1)
+y_f = np.array([50, 55, 60, 65, 75, 80, 85, 90, 95]).reshape(-1, 1)
 
 model.fit(x_f, y_f)
-
-x = np.linspace(150, 180, 50).reshape(-1, 1)
 y = model.predict(x)
 
 print(f"M: {model.coef_}\tQ: {y[0][0] - model.coef_ * x[0][0]}")
